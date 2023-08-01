f2f12cb70bfccc0376ec3eff8649860cabe3aa6b
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-08-01 13:57:29 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -24,3 +24,4 @@ file_put_contents('api_key.txt','');
 		$this->_ENDPOINTS['sentiment']['url'] = '/url/URLGetTextSentiment';
 		$this->_ENDPOINTS['sentiment']['text'] = '/text/TextGetTextSentiment';
 		$this->_ENDPOINTS['sentiment']['html'] = '/html/HTMLGetTextSentiment';
+		$this->_ENDPOINTS['sentiment_targeted']['url'] = '/url/URLGetTargetedSentiment';
