e3333a3928f656296fdcf1b3ffdfe086c8b001b0
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-08-12 15:53:54 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -33,3 +33,4 @@ file_put_contents('api_key.txt','');
 		$this->_ENDPOINTS['keywords']['text'] = '/text/TextGetRankedKeywords';
 		$this->_ENDPOINTS['concepts']['html'] = '/html/HTMLGetRankedConcepts';
 		$this->_ENDPOINTS['concepts']['text'] = '/text/TextGetRankedConcepts';
+		$this->_ENDPOINTS['concepts']['html'] = '/html/HTMLGetRankedConcepts';
