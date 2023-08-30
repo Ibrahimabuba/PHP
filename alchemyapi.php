08fe137bb8182834e6fe183fdfa180ef1672c96b
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-08-30 16:47:46 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -45,3 +45,4 @@ file_put_contents('api_key.txt','');
 		$this->_ENDPOINTS['relations']['html'] = '/html/HTMLGetRelations';
 		$this->_ENDPOINTS['language']['url']  = '/url/URLGetLanguage';
 		$this->_ENDPOINTS['language']['text'] = '/text/TextGetLanguage';
+		$this->_ENDPOINTS['language']['html'] = '/html/HTMLGetLanguage';
