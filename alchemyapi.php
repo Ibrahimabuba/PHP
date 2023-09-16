c0b992582a03ceb0babd8b230afb9ac6155e8807
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-09-16 19:42:12 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -60,3 +60,4 @@ file_put_contents('api_key.txt','');
 
 	public function entities($flavor, $data, $options) {
 		//Make sure this request supports the flavor
+		if (!array_key_exists($flavor, $this->_ENDPOINTS['entities'])) {
