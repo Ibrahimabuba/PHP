45fceff2500a8610770c7ad80711d9b11ce8f1e5
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-09-24 16:01:00 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -72,3 +72,4 @@ file_put_contents('api_key.txt','');
 
 	public function keywords($flavor, $data, $options) {
 		//Make sure this request supports the flavor
+		if (!array_key_exists($flavor, $this->_ENDPOINTS['keywords'])) {
