41bcb95a5c93f171f8775cf173bd7931b35a89b3
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-10-06 15:05:19 +0300

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -87,3 +87,5 @@ file_put_contents('api_key.txt','');
 		if (!array_key_exists($flavor, $this->_ENDPOINTS['sentiment'])) {
 			return array('status'=>'ERROR','statusInfo'=>'Sentiment analysis for ' . $flavor . ' not available');
 		}
+
+		//Add the URL encoded data to the options and analyze
