623af0262f84f15ae519a490a613bac763d58eef
Ibrahimabuba <117759782+Ibrahimabuba@users.noreply.github.com>
2023-11-08 15:22:34 +0200

    Update alchemyapi.php

--- alchemyapi.php
+++ alchemyapi.php
@@ -125,3 +125,4 @@ file_put_contents('api_key.txt','');
 	public function author($flavor, $data, $options) {
 		//Make sure this request supports the flavor
 		if (!array_key_exists($flavor, $this->_ENDPOINTS['author'])) {
+			return array('status'=>'ERROR','statusInfo'=>'Author extration for ' . $flavor . ' not available');
